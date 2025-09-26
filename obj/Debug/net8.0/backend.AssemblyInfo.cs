fa8b8c7d76a0ec45078a72aedb2a5d1751aaa038
charithasekara <86426242+charithasekara@users.noreply.github.com>
2025-09-26 13:10:40 +0530

    added books

--- obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ee5b8ddbf5e955cee22f354c8895ef9ec15bc95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbaf0aec9cbdb6f698920adb0eb6d0662d87bfe9")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
